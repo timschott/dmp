63a3d67af266efb3d49fdfa189a6d452137e1fa3
timschott <tcs9pk@virginia.edu>
2018-12-20 13:39:47 -0500

    volcano finished

--- scriptsAndDatabase/VOLCANOclean.R
+++ scriptsAndDatabase/VOLCANOclean.R
@@ -14,21 +14,18 @@ library(qdap)
 stock <- c("Title", "Type", "ID", "Unit")
 volc <- scan("rawTexts/malcolm-lowry-under-the-volcano.txt",what="character",sep="\n")
 volc_start <- 65
-volc_end <- which(volc == "Somebody threw a dead dog after him down the ravine.")
+volc_end <- 12346
 volc <- volc[volc_start:volc_end]
-
 volc <- replace_abbreviation(volc)
 volc <- gsub('_', '', perl=TRUE, volc)
-volc <- gsub('I^|II^|III^', '', perl=TRUE, volc)
+#volc <- gsub('I\n|II\n|III\n|IV\n|V\n|VI\n|VII\n', '', perl=TRUE, volc)
 volc <- gsub('M. Laruelle', 'M Laruelle', perl=TRUE, volc)
-print(length(volc))
 
 first_bite <- volc[1:2499]
 second_bite<- volc[2500:4999]
 third_bite <- volc[5000:7499]
 fourth_bite<- volc[7500:9999]
-fifth_bite <- volc[10000:12279]
-
+fifth_bite <- volc[10000:12282]
 
 volc.sents.first <- paste0(first_bite, collapse = "\n")
 volc.sents.first <- unlist(tokenize_sentences(volc.sents.first))
@@ -91,9 +88,9 @@ volc.sents <- volc.sents[-c(bad_spots)]
 print(length(volc.sents))
 
 
-volc.title <- rep("underTheVolcano", 7557)
-volc.sents.type <- rep("sentence", 7557)
-volc.sents.counter<-seq(1, 7557)
+volc.title <- rep("underTheVolcano", 7559)
+volc.sents.type <- rep("sentence", 7559)
+volc.sents.counter<-seq(1, 7559)
 volc.sents.id <- paste0("UNDER_THE_VOLCANO", "SENT_", volc.sents.counter)
 print(length(volc.sents.id))
 volc.sents.matrix <- cbind(volc.title, volc.sents.type, volc.sents.id, volc.sents)
@@ -106,6 +103,7 @@ con <- dbConnect(RSQLite::SQLite(), ":memory:", dbname="textTable.sqlite")
 
 dbWriteTable(con, "textTable", volc.sents.df, append=TRUE, row.names=FALSE)
 dbGetQuery(con, "SELECT Unit FROM textTable WHERE Type='sentence' AND Title='underTheVolcano' LIMIT 2")
+dbDisconnect(con)
 
 ####### did you see the words? 
 
@@ -119,7 +117,6 @@ volc <- volc[volc_start:volc_end]
 
 volc <- replace_abbreviation(volc)
 volc <- gsub('_', '', perl=TRUE, volc)
-volc <- gsub('I^|II^|III^', '', perl=TRUE, volc)
 volc <- gsub('M. Laruelle', 'M Laruelle', perl=TRUE, volc)
 print(length(volc))
 
@@ -150,4 +147,38 @@ dbWriteTable(con, "textTable", volc.words.df, append=TRUE, row.names=FALSE)
 dbGetQuery(con, "SELECT * FROM textTable WHERE Type= 'word' AND Title='underTheVolcano' LIMIT 10")
 dbDisconnect(con)
 
+## paras. 
 
+volc.paragraphs <- read.csv("Python_Scripts/checkCorpus/VOLC_paras.csv", stringsAsFactors = FALSE)
+volc.paragraphs <- volc.paragraphs[-c(1:49,162, 2144:2148),]
+
+colnames(volc.paragraphs) <- c("arb", "para")
+volc.paragraphs <- volc.paragraphs %>%
+  transmute(paragraph = gsub('M. Laruelle', 'M Laruelle', perl=TRUE, para))
+
+colnames(volc.paragraphs)
+volc.paragraphs <- as.data.frame(volc.paragraphs[-c(1,257,420,917, 1111,1265, 1956),], stringsAsFactors=FALSE)
+colnames(volc.paragraphs) <- c("para")
+colnames(volc.paragraphs)
+colnames(volc.paragraphs) <- c("para")
+
+
+volc.paragraphs <- volc.paragraphs %>%
+  transmute(paragraph = str_replace_all(para, "[\n]", " "))
+print(length(volc.paragraphs$paragraph))
+
+volc.title <- rep("underTheVolcano", 2086)
+volc.paras.type <- rep("paragraph", 2086)
+volc.paras.counter<-seq(1, 2086)
+volc.paras.id <- paste0("UNDER_THE_VOLCANO_", "PARAGRAPH_", volc.paras.counter)
+volc.paras.matrix <- cbind(volc.title, volc.paras.type, volc.paras.id, volc.paragraphs)
+volc.paras.df <- as.data.frame(volc.paras.matrix, stringsAsFactors = FALSE)
+colnames(volc.paras.df) <- stock
+
+con <- dbConnect(RSQLite::SQLite(), ":memory:", dbname="textTable.sqlite")
+
+dbWriteTable(con, "textTable", volc.paras.df, append=TRUE, row.names=FALSE)
+dbGetQuery(con, "SELECT Unit FROM textTable WHERE Type='paragraph' AND Title='underTheVolcano' LIMIT 2")
+dbDisconnect(con)
+
+### done.
