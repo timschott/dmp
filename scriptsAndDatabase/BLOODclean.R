b3d11c213ff8aefd0532209d12cabe51695b4d7c
timschott <tcs9pk@virginia.edu>
2018-12-25 14:08:30 -0500

    bloody hell

---
+++ scriptsAndDatabase/BLOODclean.R
@@ -0,0 +1,131 @@
+setwd("~/Documents/7thSemester/dmp/corpus")
+library("RSQLite")
+library("tokenizers")
+library("dplyr")
+library("textclean")
+library("stringr")
+library("tm")
+library(qdap)
+install.packages("rJava")
+library(rJava)
+library("openNLPdata")
+
+# blood meridian.
+
+blood <- scan("rawTexts/cormac-mccarthy-blood-meridian.txt",what="character",sep="\n")
+stock <- c("Title", "Type", "ID", "Unit")
+
+blood.start<- which(blood == "See the child. He is pale and thin, he wears a thin and ragged linen shirt. He stokes the scullery fire. Outside lie dark turned fields with rags of snow and darker woods beyond that harbor yet a few last wolves. His folk are known for hewers of wood and drawers of water but in truth his father has been a schoolmaster. He lies in drink, he quotes from poets whose names are now lost. The boy crouches by the fire and watches him.")
+blood.end <- which(blood == "In the dawn there is a man progressing over the plain by means of holes which he is making in the ground. He uses an implement with two handles and he chucks it into the hole and he enkindles the stone in the hole with his steel hole by hole striking the fire out of the rock which God has put there. On the plain behind him are the wanderers in search of bones and those who do not search and they move haltingly in the light like mechanisms whose movements are monitored with escapement and pallet so that they appear restrained by a prudence or reflectiveness which has no inner reality and they cross in their progress one by one that track of holes that runs to the rim of the visible ground and which seems less the pursuit of some continuance than the verification of a principle, a validation of sequence and causality as if each round and perfect hole owed its existence to the one before it there on that prairie upon which are the bones and the gatherers of bones and those who do not gather. He strikes fire in the hole and draws out his steel. Then they all move on again.")
+
+blood <- blood[blood.start:blood.end]
+
+blood <- replace_abbreviation(blood)
+grep("Mr.", blood)
+
+blood.paragraphs <- as.data.frame(blood, stringsAsFactors=FALSE)
+colnames(blood.paragraphs) <- c("paras")
+blood.paragraphs <- blood.paragraphs %>% 
+  transmute(paragraphs=  gsub("Mr.", "Mr", paras))
+blood.paragraphs <- blood.paragraphs %>% 
+  transmute(paras=  replace_abbreviation(paragraphs))
+print(length(blood.paragraphs$paras))
+blood.paragraphs <- blood.paragraphs %>% filter(paras!="")
+print(length(blood.paragraphs$paras))
+
+# good.
+blood.title <- rep("bloodMeridian", 2640)
+blood.para.type <- rep("paragraph", 2640)
+blood.para.counter<-seq(1, 2640)
+blood.para.id <- paste0("BLOOD_MERIDIAN_", "PARAGRAPH_", blood.para.counter)
+print(length(blood.para.id))
+
+blood.para.matrix <- cbind(blood.title, blood.para.type, blood.para.id, blood.paragraphs)
+blood.para.df <- as.data.frame(blood.para.matrix, stringsAsFactors = FALSE)
+colnames(blood.para.df) <- stock
+
+con <- dbConnect(RSQLite::SQLite(), ":memory:", dbname="textTable.sqlite")
+dbWriteTable(con, "textTable", blood.para.df, append=TRUE, row.names=FALSE)
+dbGetQuery(con, "SELECT Unit FROM textTable WHERE Type='paragraph' AND Title='bloodMeridian' LIMIT 2")
+dbDisconnect(con)
+
+
+# sick. sents!
+
+first_bite <- blood[1:2640]
+
+blood.sents.first <- paste0(first_bite, collapse = "\n")
+blood.sents.first <- unlist(tokenize_sentences(blood.sents.first))
+
+blood.sents <- c(blood.sents.first)
+blood.sents.df <- as.data.frame(blood.sents, stringsAsFactors = FALSE)
+
+bad_spots <-c(0)
+for(i in seq(1:length(blood.sents))){
+  #if the sentence ends with a punctuation mark and the next character is a lowercase, combine them,
+  # if the sequence starts with a capital letter... but for eg ha! ha! ha! don't combine
+  # so check if the first sentence starts with a lowercase as well
+  test <- substr(blood.sents[i], nchar(blood.sents[i]), nchar(blood.sents[i]))
+  test2 <- substr(blood.sents[i+1], 1, 1)
+  test3 <- substr(blood.sents[i], 1, 1)
+  if(test2 %in% c(LETTERS, letters)){
+    if(test %in% c('?', '!') && test2==tolower(test2) && test3!=tolower(test3)){
+      #print(i)
+      blood.sents[i] <- paste(blood.sents[i], blood.sents[i+1])
+      bad_spots<-append(bad_spots, i+1)
+    }
+  }
+}
+bad_spots <- bad_spots[-c(1)]
+blood.sents[bad_spots]
+blood.sents <- blood.sents[-c(bad_spots)]
+
+# sick.
+
+print(length(blood.sents))
+
+blood.title <- rep("bloodMeridian", 7568)
+blood.sents.type <- rep("sentence", 7568)
+blood.sents.counter<-seq(1, 7568)
+blood.sents.id <- paste0("BLOOD_MERIDIAN_", "SENT_", blood.sents.counter)
+print(length(blood.sents.id))
+
+blood.sents.matrix <- cbind(blood.title, blood.sents.type, blood.sents.id, blood.sents)
+blood.sents.df <- as.data.frame(blood.sents.matrix, stringsAsFactors = FALSE)
+colnames(blood.sents.df) <- stock
+con <- dbConnect(RSQLite::SQLite(), ":memory:", dbname="textTable.sqlite")
+
+dbWriteTable(con, "textTable", blood.sents.df, append=TRUE, row.names=FALSE)
+dbGetQuery(con, "SELECT Unit FROM textTable WHERE Type='sentence' AND Title='bloodMeridian' LIMIT 2")
+dbDisconnect(con)
+
+# words.
+
+blood.temp <- blood
+blood.temp <- paste(blood.temp, collapse=" ")
+blood.temp <-tolower(blood.temp)
+# a better regex that is going to maintain contractions. important! 
+
+blood.temp <- unlist(strsplit(blood.temp, "[^\\w']", perl=TRUE))
+blood.not.blanks <- which(blood.temp != "")
+blood.words <- blood.temp[blood.not.blanks]
+print(length(blood.words))
+blood.words<- blood.words[which(blood.words!="'")]
+print(length(blood.words))
+
+blood.title <- rep("bloodMeridian", 115182)
+blood.words.type <- rep("word", 115182)
+blood.words.counter <- seq(1, 115182)
+blood.words.id <- paste0("BLOOD_MERIDIAN_", "WORD_", blood.words.counter)
+
+blood.words.matrix <- cbind(blood.title, blood.words.type, blood.words.id, blood.words)
+
+blood.words.df <- as.data.frame(blood.words.matrix, stringsAsFactors = FALSE)
+
+con <- dbConnect(RSQLite::SQLite(), ":memory:", dbname="textTable.sqlite")
+colnames(blood.words.df) <- c("Title", "Type", "ID", "Unit")
+dbWriteTable(con, "textTable", blood.words.df, append=TRUE, row.names=FALSE)
+dbGetQuery(con, "SELECT * FROM textTable WHERE Type= 'word' AND Title='bloodMeridian' LIMIT 10")
+dbDisconnect(con)
+
+# blood done.
---
+++ scriptsAndDatabase/HAPPENEDclean.R
@@ -0,0 +1,174 @@
+setwd("~/Documents/7thSemester/dmp/corpus")
+library("RSQLite")
+library("tokenizers")
+library("dplyr")
+library("textclean")
+library("stringr")
+library("tm")
+library(qdap)
+# library(rJava)
+# library("openNLPdata")
+
+## something happened
+
+hell <- scan("rawTexts/joseph-heller-something-happened.txt",what="character", sep="\n")
+hell.start <- which(hell=="I get the willies when I see closed doors. Even at work, where I am doing so well now, the sight of a closed door is sometimes enough to make me dread that something horrible is happening behind it, something that is going to affect me adversely; if I am tired and dejected from a night of lies or booze or sex or just plain nerves and insomnia, I can almost smell the disaster mounting invisibly and flooding out toward me through the frosted glass panes. My hands may perspire, and my voice may come out strange. I wonder why.")
+hell.end<- which(hell=="Everyone seems pleased with the way I’ve taken command.")
+hell <- hell[hell.start:hell.end]
+# get rid of chapters; which are simple words in this book.
+grep("Nobody knows what I’ve done", hell)
+hell <- hell[-29]
+hell <- hell[-408]
+hell <- hell[-876]
+hell <- hell[-1665]
+hell <- hell[-3025]
+hell <- hell[-4391]
+hell <- hell[-4909]
+hell <- hell[-5062]
+
+hell.paragraphs <- as.data.frame(hell, stringsAsFactors=FALSE)
+colnames(hell.paragraphs) <- c("paras")
+hell.paragraphs <- hell.paragraphs %>% 
+  transmute(paragraphs=  gsub("Mr.", "Mr", paras))
+hell.paragraphs <- hell.paragraphs %>% 
+  transmute(paras=  replace_abbreviation(paragraphs))
+print(length(hell.paragraphs$paras))
+hell.paragraphs <- hell.paragraphs %>% filter(paras!="")
+print(length(hell.paragraphs$paras))
+
+# alright cool. 
+
+hell.title <- rep("somethingHappened", 5124)
+hell.para.type <- rep("paragraph", 5124)
+hell.para.counter<-seq(1, 5124)
+hell.para.id <- paste0("SOMETHING_HAPPENED_", "PARAGRAPH_", hell.para.counter)
+print(length(hell.para.id))
+
+hell.para.matrix <- cbind(hell.title, hell.para.type, hell.para.id, hell.paragraphs)
+hell.para.df <- as.data.frame(hell.para.matrix, stringsAsFactors = FALSE)
+colnames(hell.para.df) <- stock
+
+con <- dbConnect(RSQLite::SQLite(), ":memory:", dbname="textTable.sqlite")
+dbWriteTable(con, "textTable", hell.para.df, append=TRUE, row.names=FALSE)
+dbGetQuery(con, "SELECT Unit FROM textTable WHERE Type='paragraph' AND Title='somethingHappened' LIMIT 2")
+dbDisconnect(con)
+
+# cool.
+
+hell <- hell.paragraphs$paras
+print(length(hell))
+hell[1]
+
+first_bite <- hell[1:2499]
+second_bite<- hell[2500:5124]
+
+hell.sents.first <- paste0(first_bite, collapse = "\n")
+hell.sents.first <- unlist(tokenize_sentences(hell.sents.first))
+
+hell.sents.second <- paste0(second_bite, collapse = "\n")
+hell.sents.second <- unlist(tokenize_sentences(hell.sents.second))
+
+hell.sents <- c(hell.sents.first, hell.sents.second)
+hell.sents.df <- as.data.frame(hell.sents, stringsAsFactors = FALSE)
+# quote loop. 
+
+bad_spots <-c(0)
+for(i in seq(1:length(hell.sents))){
+  #if the sentence ends with a punctuation mark and the next character is a lowercase, combine them
+  test <- substr(hell.sents[i], nchar(hell.sents[i])-1, nchar(hell.sents[i]))
+  test2 <- substr(hell.sents[i+1], 1, 1)
+  if(test2 %in% c(LETTERS, letters)){
+    if(test %in% c("?”", "!”") && test2==tolower(test2)){
+      hell.sents[i] <- paste(hell.sents[i], hell.sents[i+1])
+      bad_spots<-append(bad_spots, i+1)
+    }
+  }
+}
+bad_spots <- bad_spots[-c(1)]
+
+hell.sents[bad_spots]
+hell.sents <- hell.sents[-c(bad_spots)]
+print(length(hell.sents))
+
+# no quote. 
+bad_spots <-c(0)
+for(i in seq(1:length(hell.sents))){
+  #if the sentence ends with a punctuation mark and the next character is a lowercase, combine them,
+  # if the sequence starts with a capital letter... but for eg ha! ha! ha! don't combine
+  # so check if the first sentence starts with a lowercase as well
+  test <- substr(hell.sents[i], nchar(hell.sents[i]), nchar(hell.sents[i]))
+  test2 <- substr(hell.sents[i+1], 1, 1)
+  test3 <- substr(hell.sents[i], 1, 1)
+  if(test2 %in% c(LETTERS, letters)){
+    if(test %in% c('?', '!') && test2==tolower(test2) && test3!=tolower(test3)){
+      #print(i)
+      hell.sents[i] <- paste(hell.sents[i], hell.sents[i+1])
+      bad_spots<-append(bad_spots, i+1)
+    }
+  }
+}
+bad_spots <- bad_spots[-c(1)]
+hell.sents[bad_spots]
+hell.sents <- hell.sents[-c(bad_spots)]
+print(length(hell.sents))
+hell.sents[7514] <-paste(hell.sents[7514], hell.sents[7515])
+hell.sents[7515] <- ""
+hell.sents[8623] <- paste(hell.sents[8623], hell.sents[8624])
+hell.sents[8624] <- ""
+hell.sents[9061:9062]
+hell.sents[9061] <- paste(hell.sents[9061], hell.sents[9062])
+hell.sents[9062] <- ""
+hell.sents <- hell.sents[hell.sents!=""]
+print(length(hell.sents))
+
+           # lots of sents. 
+hell.title <- rep("somethingHappened", 14884)
+hell.sents.type <- rep("sentence", 14884)
+hell.sents.counter<-seq(1, 14884)
+hell.sents.id <- paste0("SOMETHING_HAPPENED_", "SENT_", hell.sents.counter)
+print(length(hell.sents.id))
+
+hell.sents.matrix <- cbind(hell.title, hell.sents.type, hell.sents.id, hell.sents)
+hell.sents.df <- as.data.frame(hell.sents.matrix, stringsAsFactors = FALSE)
+colnames(hell.sents.df) <- stock
+con <- dbConnect(RSQLite::SQLite(), ":memory:", dbname="textTable.sqlite")
+
+dbWriteTable(con, "textTable", hell.sents.df, append=TRUE, row.names=FALSE)
+dbGetQuery(con, "SELECT Unit FROM textTable WHERE Type='sentence' AND Title='somethingHappened' LIMIT 2")
+dbDisconnect(con)
+
+# once more, with feeling.
+
+# words.
+
+hell.temp <- hell
+hell.temp <- paste(hell.temp, collapse=" ")
+hell.temp <-tolower(hell.temp)
+# a better regex that is going to maintain contractions. important! 
+
+hell.temp <- unlist(strsplit(hell.temp, "[^\\w’]", perl=TRUE))
+hell.not.blanks <- which(hell.temp != "")
+hell.words <- hell.temp[hell.not.blanks]
+print(length(hell.words))
+
+hell.words<- hell.words[which(hell.words!="’")]
+print(length(hell.words))
+hell.words[1:100]
+
+
+hell.title <- rep("somethingHappened", 190267)
+hell.words.type <- rep("word", 190267)
+hell.words.counter <- seq(1, 190267)
+hell.words.id <- paste0("SOMETHING_HAPPENED_", "WORD_", hell.words.counter)
+
+hell.words.matrix <- cbind(hell.title, hell.words.type, hell.words.id, hell.words)
+
+hell.words.df <- as.data.frame(hell.words.matrix, stringsAsFactors = FALSE)
+
+con <- dbConnect(RSQLite::SQLite(), ":memory:", dbname="textTable.sqlite")
+colnames(hell.words.df) <- c("Title", "Type", "ID", "Unit")
+dbWriteTable(con, "textTable", hell.words.df, append=TRUE, row.names=FALSE)
+dbGetQuery(con, "SELECT * FROM textTable WHERE Type= 'word' AND Title='somethingHappened' LIMIT 10")
+dbDisconnect(con)
+
+# hell done. 
