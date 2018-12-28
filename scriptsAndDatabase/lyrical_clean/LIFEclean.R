5ece9be9573f1eac61bda6dbc87a9d1ade7d333d
timschott <tcs9pk@virginia.edu>
2018-12-28 16:54:34 -0500

    idiot fix:

--- scriptsAndDatabase/lyrical_clean/LIFEclean.R
+++ scriptsAndDatabase/lyrical_clean/LIFEclean.R
@@ -10,8 +10,8 @@ library(qdap)
 # library("openNLPdata")
 # paras first. like pale fire. 
 
-stock <- c("Title", "Type", "ID", "Unit")
-life <- scan("rawTexts/j-m-coetzee-michael-k.txt",what="character", sep="\n")
+stock <- c("Title", "Type", "ID", "Unit", "Label")
+life <- scan("rawTexts/lyrical/j-m-coetzee-michael-k.txt",what="character", sep="\n")
 life.end<- which(life=="that they recognized who it was.")
 life <- life[5:785]
 chaps <- grep("^[0-9]+$", life)
@@ -19,25 +19,28 @@ life <- life[-chaps]
 grep("^TWO$", life)
 life <- life[-547]
 grep("Mrs.", life)
-life <- gsub("Mr.", "Mr", perl=TRUE, life)
+life <- gsub("Mr\\.", "Mr", perl=TRUE, life)
 life <- replace_abbreviation(life)
 
-print(length(life))
+# print(length(life))
 life <- life[life!=""]
-print(length(life))
+# print(length(life))
 life.paragraphs <- life
 life.title <- rep("lifeAndTimesOfMichaelK", 762)
 life.para.type <- rep("paragraph", 762)
 life.para.counter<-seq(1, 762)
+life.label <- rep("1", 762)
 life.para.id <- paste0("LIFE_AND_TIMES_OF_MICHAEL_K_", "PARAGRAPH_", life.para.counter)
-print(length(life.para.id))
-life.para.matrix <- cbind(life.title, life.para.type, life.para.id, life.paragraphs)
+# print(length(life.para.id))
+life.para.matrix <- cbind(life.title, life.para.type, life.para.id, life.paragraphs, life.label)
 life.para.df <- as.data.frame(life.para.matrix, stringsAsFactors = FALSE)
 colnames(life.para.df) <- stock
 
 con <- dbConnect(RSQLite::SQLite(), ":memory:", dbname="textTable.sqlite")
 dbWriteTable(con, "textTable", life.para.df, append=TRUE, row.names=FALSE)
 dbGetQuery(con, "SELECT Unit FROM textTable WHERE Type='paragraph' AND Title='lifeAndTimesOfMichaelK' LIMIT 2")
+# the saddest query there ever was.
+ # dbExecute(con, "DELETE FROM textTable WHERE Title='gravitysRainbow' OR Title='absalomAbsalom' OR Title='bloodMeridian' OR Title= 'somethingHappened' OR Title= 'wideSargassoSea' OR Title= 'lifeAndTimesOfMichaelK' OR Title='mrsDalloway' OR Title= 'theShriekingPit' OR Title= 'theMoonstone'")
 dbDisconnect(con)
 
 # sents. 
@@ -99,13 +102,15 @@ print(length(life.sents))
 # cool. 
 
 life.sents[1234:1244]
-
+life.sents[grep("Mr", life.sents)]
 life.title <- rep("lifeAndTimesOfMichaelK", 4253)
 life.sents.type <- rep("sentence", 4253)
 life.sents.counter<-seq(1, 4253)
+life.label <- rep("1", 4253)
 life.sents.id <- paste0("LIFE_AND_TIMES_OF_MICHAEL_K_", "SENT_", life.sents.counter)
+
 print(length(life.sents.id))
-life.sents.matrix <- cbind(life.title, life.sents.type, life.sents.id, life.sents)
+life.sents.matrix <- cbind(life.title, life.sents.type, life.sents.id, life.sents, life.label)
 life.sents.df <- as.data.frame(life.sents.matrix, stringsAsFactors = FALSE)
 colnames(life.sents.df) <- stock
 con <- dbConnect(RSQLite::SQLite(), ":memory:", dbname="textTable.sqlite")
@@ -131,17 +136,17 @@ life.words <- life.words[which(life.words !="")]
 print(length(life.words))
 
 #db commit. 
-life.title <- rep("lifeAndTimesOfMichaelK", 66345)
-life.words.type <- rep("word", 66345)
-life.words.counter <- seq(1, 66345)
+life.title <- rep("lifeAndTimesOfMichaelK", 66354)
+life.words.type <- rep("word", 66354)
+life.words.counter <- seq(1, 66354)
 life.words.id <- paste0("LIFE_AND_TIMES_OF_MICHAEL_K_", "WORD_", life.words.counter)
-
-life.words.matrix <- cbind(life.title, life.words.type, life.words.id, life.words)
+life.label <- rep("1", 66354)
+life.words.matrix <- cbind(life.title, life.words.type, life.words.id, life.words, life.label)
 
 life.words.df <- as.data.frame(life.words.matrix, stringsAsFactors = FALSE)
 
 con <- dbConnect(RSQLite::SQLite(), ":memory:", dbname="textTable.sqlite")
-colnames(life.words.df) <- c("Title", "Type", "ID", "Unit")
+colnames(life.words.df) <- c("Title", "Type", "ID", "Unit", "Label")
 dbWriteTable(con, "textTable", life.words.df, append=TRUE, row.names=FALSE)
 dbGetQuery(con, "SELECT * FROM textTable WHERE Type= 'word' AND Title='lifeAndTimesOfMichaelK' LIMIT 10")
 dbDisconnect(con)
