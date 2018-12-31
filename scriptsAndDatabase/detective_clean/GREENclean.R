c84f931cae33258e0e28394724c107298af261d6
timschott <tcs9pk@virginia.edu>
2018-12-31 10:34:48 -0500

    green is cleaned

--- scriptsAndDatabase/detective_clean/GREENclean.R
+++ scriptsAndDatabase/detective_clean/GREENclean.R
@@ -20,18 +20,26 @@ green.chaps<-spots[c(1,3,5,7,9,12,14,16,18,26,28,37,40,42,44,
 green.content<-spots[c(2,4,6,8,11,13,15,17,19,27,30,38,41,43,45,
                      47,49,51,53,55,57,63,65,67,69,72,74,76,
                      79,81,85,88,92,96,98,100,105)]
-green.books<-spots[c(39,70, 89)]
+green.chaps
+green.content
+
+green.remove <- c(39,70,74, 75, 76, 77, 87:92, 331:333, 407:412, 488:492, 518:520,
+                  575, 576, 769:774, 847:850, 941:943, 986:994, 1044:1047, 1113:1115,
+                  1226:1228, 1288:1291, 1327:1331, 1431:1433, 1493:1495, 1546:1550, 1584:1588,
+                  1584:1588, 1672:1674, 1878:1882, 1961:1965, 2058:2060, 2145:2147,
+                  2256:2260, 2298: 2300, 2363:2365, 2386: 2388, 2444:2445, 2450:2456,
+                  2749:2752, 2782: 2786, 3076:3078, 3103:3108, 3145:3153, 3194:3205)
 # hold out. green.bad <- spots[94]
 green.chaps[1]
 green.content[1]
 green[green.chaps[1]:(green.content[1]-1)]
+
 green[green.content]
 green.content
-toRemove <-c(0)
 
-green <- green[-green.books]
+green <- green[-green.remove]
 grep("BOOK IV", green)
-green <- green[-c(976, 2055, 2548)]
+green <- green[-c(2647)]
 print(length(green))
 
 green.paragraphs <- as.data.frame(green, stringsAsFactors=FALSE)
@@ -68,19 +76,128 @@ green.paragraphs <- green.paragraphs %>%
   filter(paragraphs!="  ")
 print(length(green.paragraphs$paragraphs))
 
+green.title <- rep("theLeavenworthCase", 3066)
+green.para.type <- rep("paragraph",3066)
+green.para.counter<-seq(1, 3066)
+green.para.id <- paste0("THE_LEAVENWORTH_CASE_", "PARAGRAPH_", green.para.counter)
+green.label <- rep("0", 3066)
+print(length(green.para.id))
 
-rayn.title <- rep("theRaynerSladeAmalgamation", 3093)
-rayn.para.type <- rep("paragraph",3093)
-rayn.para.counter<-seq(1, 1717)
-rayn.para.id <- paste0("THE_RAYNER_SLADE_AMALGAMATION_", "PARAGRAPH_", rayn.para.counter)
-rayn.label <- rep("0", 1717)
-print(length(rayn.para.id))
-
-rayn.para.matrix <- cbind(rayn.title, rayn.para.type, rayn.para.id, rayn.paragraphs, rayn.label)
-rayn.para.df <- as.data.frame(rayn.para.matrix, stringsAsFactors = FALSE)
+green.para.matrix <- cbind(green.title, green.para.type, green.para.id, green.paragraphs, green.label)
+green.para.df <- as.data.frame(green.para.matrix, stringsAsFactors = FALSE)
 stock <- c("Title", "Type", "ID", "Unit", "Label")
-colnames(rayn.para.df) <- stock
+colnames(green.para.df) <- stock
 con <- dbConnect(RSQLite::SQLite(), ":memory:", dbname="textTable.sqlite")
-dbWriteTable(con, "textTable", rayn.para.df, append=TRUE, row.names=FALSE)
-dbGetQuery(con, "SELECT Unit FROM textTable WHERE Type='paragraph' AND Title='theRaynerSladeAmalgamation' LIMIT 2")
+dbWriteTable(con, "textTable", green.para.df, append=TRUE, row.names=FALSE)
+dbGetQuery(con, "SELECT Unit FROM textTable WHERE Type='paragraph' AND Title='theLeavenworthCase' LIMIT 2")
+dbDisconnect(con)
+
+# sents.
+
+green <- green.paragraphs$paragraphs
+
+first_bite <- green[1:3066]
+
+green.sents.first <- paste0(first_bite, collapse = "\n")
+green.sents.first <- unlist(tokenize_sentences(green.sents.first))
+
+green.sents <- c(green.sents.first)
+green.sents.df <- as.data.frame(green.sents, stringsAsFactors = FALSE)
+
+print(length(green.sents.df$green.sents))
+
+
+bad_spots <-c(0)
+for(i in seq(1:length(green.sents))){
+  #if the sentence ends with a punctuation mark and the next character is a lowercase, combine them,
+  # if the sequence starts with a capital letter... but for eg ha! ha! ha! don't combine
+  # so check if the first sentence starts with a lowercase as well
+  test <- substr(green.sents[i], nchar(green.sents[i]), nchar(green.sents[i]))
+  test2 <- substr(green.sents[i+1], 1, 1)
+  test3 <- substr(green.sents[i], 1, 1)
+  if(test2 %in% c(LETTERS, letters)){
+    if(test %in% c('?', '!') && test2==tolower(test2) && test3!=tolower(test3)){
+      #print(i)
+      green.sents[i] <- paste(green.sents[i], green.sents[i+1])
+      bad_spots<-append(bad_spots, i+1)
+    }
+  }
+}
+bad_spots <- bad_spots[-c(1)]
+green.sents[bad_spots]
+green.sents <- green.sents[-c(bad_spots)]
+
+print(length(green.sents))
+
+bad_spots <-c(0)
+for(i in seq(1:length(green.sents))){
+  #if the sentence ends with a punctuation mark and the next character is a lowercase, combine them
+  test <- substr(green.sents[i], nchar(green.sents[i])-1, nchar(green.sents[i]))
+  test2 <- substr(green.sents[i+1], 1, 1)
+  if(test2 %in% c(LETTERS, letters)){
+    if((test %in% c('?”', '!”') && test2==tolower(test2))){
+      #print(i)
+      green.sents[i] <- paste(green.sents[i], green.sents[i+1])
+      bad_spots<-append(bad_spots, i+1)
+    }
+  }
+}
+
+green.sents[bad_spots]
+green.sents <- green.sents[-c(bad_spots)]
+green.sents <- green.sents[green.sents!=""]
+print(length(green.sents))
+
+green.sents.df <- as.data.frame(green.sents, stringsAsFactors = FALSE)
+
+green.title <- rep("theLeavenworthCase", 6133)
+green.sents.type <- rep("sentence", 6133)
+green.sents.counter<-seq(1, 6133)
+green.sents.id <- paste0("THE_LEAVENWORTH_CASE_", "SENT_", green.sents.counter)
+green.label <- rep("0", 6133)
+print(length(green.sents.id))
+
+green.sents.matrix <- cbind(green.title, green.sents.type, green.sents.id, green.sents, green.label)
+green.sents.df <- as.data.frame(green.sents.matrix, stringsAsFactors = FALSE)
+stock <- c("Title", "Type", "ID", "Unit", "Label")
+colnames(green.sents.df) <- stock
+con <- dbConnect(RSQLite::SQLite(), ":memory:", dbname="textTable.sqlite")
+
+dbWriteTable(con, "textTable", green.sents.df, append=TRUE, row.names=FALSE)
+dbGetQuery(con, "SELECT Unit FROM textTable WHERE Type='sentence' AND Title='theLeavenworthCase' LIMIT 2")
+dbDisconnect(con)
+
+# words
+
+green.temp <- green
+green.temp <- paste(green.temp, collapse=" ")
+green.temp <-tolower(green.temp)
+# a better regex that is going to maintain contractions. important! 
+
+green.temp <- unlist(strsplit(green.temp, "[^\\w’]", perl=TRUE))
+green.not.blanks <- which(green.temp != "")
+green.words <- green.temp[green.not.blanks]
+print(length(green.words))
+
+green.words<- green.words[which(green.words!="^’")]
+green.words<- green.words[which(green.words!="’")]
+print(length(green.words))
+
+green.words.df <- as.data.frame(green.words, stringsAsFactors = FALSE)
+
+green.title <- rep("theLeavenworthCase", 110218)
+green.words.type <- rep("word", 110218)
+green.words.counter <- seq(1, 110218)
+green.words.id <- paste0("THE_LEAVENWORTH_CASE_", "WORD_", green.words.counter)
+green.label<- rep("0", 110218)
+green.words.matrix <- cbind(green.title, green.words.type, green.words.id, green.words, green.label)
+
+green.words.df <- as.data.frame(green.words.matrix, stringsAsFactors = FALSE)
+
+stock <- c("Title", "Type", "ID", "Unit", "Label")
+colnames(green.words.df) <- c("Title", "Type", "ID", "Unit", "Label")
+con <- dbConnect(RSQLite::SQLite(), ":memory:", dbname="textTable.sqlite")
+dbWriteTable(con, "textTable", green.words.df, append=TRUE, row.names=FALSE)
+dbGetQuery(con, "SELECT * FROM textTable WHERE Type= 'word' AND Title='theLeavenworthCase' LIMIT 10")
+dbGetQuery(con, "SELECT COUNT(*) FROM textTable WHERE Type='word' and Label='0'")
 dbDisconnect(con)
