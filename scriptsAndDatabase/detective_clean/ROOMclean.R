dc8d3b1034ac58bfd274356f9611b7ea30d454d5
timschott <tcs9pk@virginia.edu>
2019-01-02 13:57:48 -0500

    from one room to the next

--- scriptsAndDatabase/detective_clean/ROOMclean.R
+++ scriptsAndDatabase/detective_clean/ROOMclean.R
@@ -74,9 +74,9 @@ dbDisconnect(con)
 
 room <- room.paragraphs$paragraphs
 
-# sents 
-
+# sents
 first_bite <- room[1:758]
+first_bite <- gsub("No\\.", "No", perl=TRUE, first_bite)
 
 room.sents.first <- paste0(first_bite, collapse = "\n")
 room.sents.first <- unlist(tokenize_sentences(room.sents.first))
@@ -85,3 +85,76 @@ room.sents <- c(room.sents.first)
 room.sents.df <- as.data.frame(room.sents, stringsAsFactors = FALSE)
 
 print(length(room.sents.df$room.sents))
+
+bad_spots <-c(0)
+for(i in seq(1:length(room.sents))){
+  #if the sentence ends with a punctuation mark and the next character is a lowercase, combine them,
+  # if the sequence starts with a capital letter... but for eg ha! ha! ha! don't combine
+  # so check if the first sentence starts with a lowercase as well
+  test <- substr(room.sents[i], nchar(room.sents[i]), nchar(room.sents[i]))
+  test2 <- substr(room.sents[i+1], 1, 1)
+  test3 <- substr(room.sents[i], 1, 1)
+  if(test2 %in% c(LETTERS, letters)){
+    if(test %in% c('?', '!') && test2==tolower(test2) && test3!=tolower(test3)){
+      #print(i)
+      room.sents[i] <- paste(room.sents[i], room.sents[i+1])
+      bad_spots<-append(bad_spots, i+1)
+    }
+  }
+}
+bad_spots <- bad_spots[-c(1)]
+room.sents[bad_spots]
+room.sents <- room.sents[-c(bad_spots)]
+room.sents <- room.sents[room.sents!=""]
+print(length(room.sents))
+
+room.sents[1999:2005]
+
+room.title <- rep("theMysteryOfRoom75", 2819)
+room.sents.type <- rep("sentence", 2819)
+room.sents.counter<-seq(1, 2819)
+room.sents.id <- paste0("THE_MYSTERY_OF_ROOM_75_", "SENT_", room.sents.counter)
+room.label <- rep("0", 2819)
+print(length(room.sents.id))
+
+room.sents.matrix <- cbind(room.title, room.sents.type, room.sents.id, room.sents, room.label)
+room.sents.df <- as.data.frame(room.sents.matrix, stringsAsFactors = FALSE)
+stock <- c("Title", "Type", "ID", "Unit", "Label")
+colnames(room.sents.df) <- stock
+con <- dbConnect(RSQLite::SQLite(), ":memory:", dbname="textTable.sqlite")
+
+dbWriteTable(con, "textTable", room.sents.df, append=TRUE, row.names=FALSE)
+dbGetQuery(con, "SELECT Unit FROM textTable WHERE Type='sentence' AND Title='theMysteryOfRoom75' LIMIT 2")
+dbDisconnect(con)
+# words. 
+room.temp <- room
+room.temp <- paste(room.temp, collapse=" ")
+room.temp <-tolower(room.temp)
+# a better regex that is going to maintain contractions. important! 
+
+room.temp <- unlist(strsplit(room.temp, "[^\\w']", perl=TRUE))
+room.not.blanks <- which(room.temp != "")
+room.words <- room.temp[room.not.blanks]
+print(length(room.words))
+room.words<- room.words[which(room.words!="^'")]
+room.words<- room.words[which(room.words!="'")]
+room.words<- room.words[which(room.words!="''")]
+print(length(room.words))
+room.words.df <- as.data.frame(room.words, stringsAsFactors = FALSE)
+
+room.title <- rep("theMysteryOfRoom75", 48754)
+room.words.type <- rep("word", 48754)
+room.words.counter <- seq(1, 48754)
+room.words.id <- paste0("THE_MYSTERY_OF_ROOM_75_", "WORD_", room.words.counter)
+room.label<- rep("0", 48754)
+room.words.matrix <- cbind(room.title, room.words.type, room.words.id, room.words, room.label)
+
+room.words.df <- as.data.frame(room.words.matrix, stringsAsFactors = FALSE)
+
+stock <- c("Title", "Type", "ID", "Unit", "Label")
+colnames(room.words.df) <- c("Title", "Type", "ID", "Unit", "Label")
+con <- dbConnect(RSQLite::SQLite(), ":memory:", dbname="textTable.sqlite")
+dbWriteTable(con, "textTable", room.words.df, append=TRUE, row.names=FALSE)
+dbGetQuery(con, "SELECT * FROM textTable WHERE Type= 'word' AND Title='theMysteryOfRoom75' LIMIT 10")
+dbGetQuery(con, "SELECT COUNT(*) FROM textTable WHERE Type='word' and Label='0'")
+dbDisconnect(con)
