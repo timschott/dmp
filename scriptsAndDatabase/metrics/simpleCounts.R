4f51f39e0c2c809448da34daa15b39cc6ac0bf9b
timschott <tcs9pk@virginia.edu>
2019-02-05 11:04:24 -0500

    question and exclamation counts

--- scriptsAndDatabase/metrics/simpleCounts.R
+++ scriptsAndDatabase/metrics/simpleCounts.R
@@ -10,7 +10,7 @@ library("tm")
 library(qdap)
 
 ### simple counts.
-# ! and ? and -- [em-dash] and percent of words that are numbers
+# ! and ?
 
 big_boy <- read.csv('starts.csv', stringsAsFactors = FALSE)
 
@@ -23,37 +23,15 @@ dbDisconnect(con)
 
 detective_excl_vec <- c(0)
 detective_question_vec <- c(0)
-detective_dash_vec <- c(0)
-detective_dash_vec_2 <- c(0)
 
 for(i in seq(from =1, to=24)){
   sents <- filter(detective_sent_df, Title==detective_titles[i])
   sents <- sents$Unit
   detective_excl_vec <- append(detective_excl_vec, sum(str_count(sents, "\\!")))
   detective_question_vec <- append(detective_question_vec, sum(str_count(sents, "\\?")))
-  detective_dash_vec <- append(detective_dash_vec, sum(str_count(sents, "—")))
-  if(i==15 || i==12 || i==10 || i==24){
-    print(sum(str_count(sents, "--")))
-    # weird temp appending 
-    detective_dash_vec_2 <- append(detective_dash_vec_2, sum(str_count(sents, "--")))
-  }
 }
-detective_excl_vec <- detective_comma_vec[-c(1)]
+detective_excl_vec <- detective_excl_vec[-c(1)]
 detective_question_vec <- detective_question_vec[-c(1)]
-detective_dash_vec <- detective_dash_vec[-c(1)]
-detective_dash_vec_2 <- detective_dash_vec_2[-c(1)]
-
-detective_dash_vec[10] <- detective_dash_vec_2[1]
-detective_dash_vec[12] <- detective_dash_vec_2[2]
-detective_dash_vec[15] <- detective_dash_vec_2[3]
-detective_dash_vec[24] <- detective_dash_vec_2[4]
-
-room <- filter(detective_sent_df, Title==detective_titles[15])
-# use -- for 15 and 12 and 10 and 24
-sum(which(grep("--", room$Unit) %in% room$Unit))
-
-test <- c("Hello -- there --", "-- ok", "good --")
-str_count(test, "--")
 
 # let's do per sentence as not 
 
@@ -68,15 +46,12 @@ dbDisconnect(con)
 
 lyrical_excl_vec <- c(0)
 lyrical_question_vec <- c(0)
-lyrical_dash_vec <- c(0)
-# lyrical_dash_vec_2 <- c(0)
 
 for(i in seq(from =1, to=26)){
   sents <- filter(lyrical_sent_df, Title==lyrical_titles[i])
   sents <- sents$Unit
   lyrical_excl_vec <- append(lyrical_excl_vec, sum(str_count(sents, "\\!")))
   lyrical_question_vec <- append(lyrical_question_vec, sum(str_count(sents, "\\?")))
-  lyrical_dash_vec <- append(lyrical_dash_vec, sum(str_count(sents, "—")))
 }
 lyrical_titles
 # some shockingly low numbers here for mccarthy.
@@ -84,4 +59,9 @@ lyrical_excl_vec <- lyrical_excl_vec[-c(1)]
 # makes sense that eureka, a weird essay of sorts, has low # of questions 
 lyrical_question_vec <- lyrical_question_vec[-c(1)]
 
-lyrical_dash_vec
+question_vec <- c(lyrical_question_vec, detective_question_vec)
+exclamation_vec <- c(lyrical_excl_vec, detective_excl_vec)
+
+big_boy <- as.data.frame(cbind(big_boy, question_vec, exclamation_vec), stringsAsFactors = FALSE)
+colnames(big_boy)
+write.csv(big_boy,'starts.csv')
