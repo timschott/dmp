4f8b03d81beee3a5e128091cf46d10121c541129
timschott <tcs9pk@virginia.edu>
2019-03-11 14:52:58 -0700

    i have a plot

--- scriptsAndDatabase/sup/SVM_PLOT_TRY.R
+++ scriptsAndDatabase/sup/SVM_PLOT_TRY.R
@@ -1,6 +1,5 @@
 w <- t(smaller_svm$coefs) %*% smaller_svm$SV
 
-Once we have the weight vector, we can calculate the slope and intercept of the predicted decision boundary as follows:
   #calculate slope
 slope_1 <- -w[1]/w[2]
 slope_1
@@ -13,7 +12,6 @@ culled_sv <- sub[smaller_svm$index,]
 
 p <- ggplot(data=sub, aes(x=dialogue_freq,y=consecutive_repeat_freq_vec,color=label2)) + geom_point()+ scale_colour_manual(values=c("red","blue"))
 p <- p + geom_point(data=culled_sv,aes(x=dialogue_freq,y=consecutive_repeat_freq_vec),colour="purple",size = 4,alpha=0.5)
-p<- p + geom_abline(slope=slope_1,intercept = intercept_1-1/w[1], linetype="dashed")+
-  geom_abline(slope=slope_1,intercept = intercept_1) +
-  geom_abline(slope=slope_1,intercept = intercept_1+1/w[1], linetype="dashed")
+p<- p + geom_abline(slope=slope_1,intercept = intercept_1-1/w[2] + 2*intercept_1-1/w[1], linetype="solid")+
+  geom_abline(slope=slope_1,intercept = intercept_1-1/w[2], linetype="dashed")
 p
--- scriptsAndDatabase/sup/SVM_final.R
+++ scriptsAndDatabase/sup/SVM_final.R
@@ -27,36 +27,26 @@ normalized_df$label2 <- as.factor(normalized_df$label2)
 culled <- normalized_df %>% dplyr::select(c(i_freq, dialogue_freq,
                                             consecutive_repeat_freq_vec,
                                             self_freq, perceive_freq, label2))
-# free 21 savage
+
+grid <- expand.grid(C = c(0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2,5,10, 15, 20, 300))
+
+# free ..25 savage ?
 set.seed(25)
 
-partition <- createDataPartition(y = culled$label2, p= 0.80, list = FALSE)
-training <- culled[partition,]
-testing <- culled[-partition,]
-
-trctrl <- trainControl(method="LOOCV")
-grid <- expand.grid(C = c(0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2,5,10, 15, 20, 300))
-svm_Linear_Grid <- train(label2 ~., data = training, method = "svmLinear",
-                         trControl=trctrl,
-                         tuneGrid = grid)
-
-
 for(i in (seq(1,16))){
-  fit <- svm(label2 ~., data = training, kernel = "linear",
-             cross=41,
+  fit <- svm(label2 ~., data = culled, kernel = "linear",
+             cross=50,
              cost = grid$C[i])
   print(length(fit$SV)/5)
 }
 
-fit <- svm(label2 ~., data = training, kernel = "linear",
-           cross=41,
-           cost = 10)
+fit <- svm(label2 ~., data = culled, kernel = "linear",
+           cross=50,
+           cost = 15)
 
-
-pred = predict(fit, testing)
-table(Actual=testing$label2, Fitted=pred)
 
 w = t(fit$coefs) %*% fit$SV
+sort(w)
 # alright so the most consequential are dialgoue and anaphora
 sub <- culled %>% dplyr::select(dialogue_freq, consecutive_repeat_freq_vec,label2)
 # sub_test <- testing %>% dplyr::select(dialogue_freq, consecutive_repeat_freq_vec,label2)
@@ -68,43 +58,31 @@ test_svm <- fit <- svm(label2 ~., data = sub, kernel = "linear",
 print(length(fit$SV)/2)
 }
 
-test_svm <- svm(label2 ~., data = sub, kernel = "linear",
-           cross=41,
+smaller_svm <- svm(label2 ~., data = sub, kernel = "linear",
+           cross=50,
            cost = 5)
 # parameter coefficients
-w = t(test_svm$coefs) %*% test_svm$SV
+small_w = t(smaller_svm$coefs) %*% smaller_svm$SV
 
 # plot
 
-plot(test_svm, sub)
+plot(smaller_svm, sub, svSymbol="x")
 
 # confusion matrix for subset 
 
-test_results <- predict(test_svm, newdata = sub_test)
-confusionMatrix(test_results, sub_test$label2)
+# manual coefficient calc. 
+t(small_w %*% t(as.matrix(sub[,-3]))) - smaller_svm$rho
+# for one row
+t(small_w %*% t(as.matrix(sub[1,-3]))) - smaller_svm$rho[1]
+b <- -1 * smaller_svm$rho
+(small_w %*% (as.matrix(sub[,-3])) + b) / sqrt(small_w %*% t(small_w))
 
-# manual
-t(w %*% t(as.matrix(sub[,-3]))) - test_svm$rho
-
+(small_w %*% sub[1,-3]) + b
 
 # old 
-test_pred_grid <- predict(test_svm, newdata = testing)
-confusionMatrix(test_pred_grid, testing$label2)
-
-test_pred_grid
-testing$label2
-testing$i_freq
-
-normalized_df$i_freq
-
-library(gmodels)
-CrossTable(testing$label2, test_pred_grid,
-           prop.chisq = FALSE, prop.c = FALSE, prop.r = FALSE,
-           dnn = c('actual label', 'predicted label'))
-
 ## We can also use ROC curves.  How is the ROC curve created?
 # the second column is P(A).
-RC <- roc(testing$label2,test_pred_grid)
+RC <- roc(sub$label2,test_pred_grid)
 plot(RC, legacy.axes = TRUE)
 
 tuned_svm <- tune(svm, label2~., data = culled,
@@ -113,18 +91,6 @@ tuned_svm <- tune(svm, label2~., data = culled,
 tuned_svm$best.model
 tuned_svm$best.performance
 
-# https://datascienceplus.com/understanding-linear-svm-with-r/
-print(tuned_svm)
-svm_good_model<-svm(label2~., data=training, kernel="linear",cost=tuned_svm$best.parameters$cost, gamma=tuned_svm$best.parameters$gamma, probability=TRUE)
-y_pred_prob<-predict(svm_good_model,newdata = testing, probability=TRUE)
-table(testing$label2,y_pred_prob)
-attr(y_pred_prob, "probabilities")[,2]
-library(gmodels)
-CrossTable(y_pred,testing$label2,prop.chisq = FALSE)
-
-library(pROC)
-r =roc(testing$label2, attr(y_pred_prob, "probabilities")[,2])
-plot(r)
 
 
 
