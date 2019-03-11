2551a0e899775556c7ed7d1aac250ad088e5430f
timschott <tcs9pk@virginia.edu>
2019-03-11 14:43:09 -0700

    plodding through plots

---
+++ scriptsAndDatabase/sup/SVM_PLOT_TRY.R
@@ -0,0 +1,19 @@
+w <- t(smaller_svm$coefs) %*% smaller_svm$SV
+
+Once we have the weight vector, we can calculate the slope and intercept of the predicted decision boundary as follows:
+  #calculate slope
+slope_1 <- -w[1]/w[2]
+slope_1
+
+#calculate intercept
+intercept_1 <- smaller_svm$rho/w[2]
+
+#sv's
+culled_sv <- sub[smaller_svm$index,]
+
+p <- ggplot(data=sub, aes(x=dialogue_freq,y=consecutive_repeat_freq_vec,color=label2)) + geom_point()+ scale_colour_manual(values=c("red","blue"))
+p <- p + geom_point(data=culled_sv,aes(x=dialogue_freq,y=consecutive_repeat_freq_vec),colour="purple",size = 4,alpha=0.5)
+p<- p + geom_abline(slope=slope_1,intercept = intercept_1-1/w[1], linetype="dashed")+
+  geom_abline(slope=slope_1,intercept = intercept_1) +
+  geom_abline(slope=slope_1,intercept = intercept_1+1/w[1], linetype="dashed")
+p
