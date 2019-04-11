eea558d49731febfdcbe0ce0d4cf728668041077
timschott <tcs9pk@virginia.edu>
2019-04-11 12:43:48 -0400

    plotting

--- Python_Scripts/MATTR/sound_and_the_fury_syllable_work.py
+++ Python_Scripts/MATTR/sound_and_the_fury_syllable_work.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pylab as plt
+
 
 def read_file(filename):
 
@@ -28,4 +31,43 @@ def syllable_count(word):
         count += 1
     return count
 
+def keep_track(words):
 
+    syll_counts = []
+
+    for word in words:
+        syll_counts.append(syllable_count(word))
+
+    return syll_counts
+
+
+if __name__ == "__main__":
+    sound = read_file("quent.txt")
+    results = keep_track(sound)
+
+    print results
+    print(len(results))
+    #858 total words
+    #1172 total syllables
+    print(sum(results))
+    polycount = 0
+
+    for i in results:
+        if i!=1:
+            polycount+=1
+
+    print 'poly ' + str(polycount)
+    monocount =  1172 - polycount
+    print 'mono ' + str(monocount)
+
+    row = np.array(results)
+    row = row.astype(float)
+    x = np.arange(1, len(row) + 1, 1)
+    print x
+    plt.figure(figsize=(9, 9))
+
+    plt.plot(x, row)
+    plt.title("Syllables Across Quentin's Last Monologue")
+    plt.savefig("quent_plot.png", dpi=500)
+    plt.gcf().clear()
+    plt.show()
